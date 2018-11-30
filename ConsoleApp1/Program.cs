25a3eb82c31d1f26b84c996e099042373064c2df
Shlomo Gordon <sgordon16@student.touro.edu>
2018-11-29 23:14:20 -0500

    added files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using MCON_368_Asgn04;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double SumTotal;
+            Sale[] salesList = new Sale[] { new Sale("Apple", "Marry", .75, 5, "96 Cedar Rd.", true),
+                new Sale("Coffe", "John", 2.5, 2, "36 Main Ave.", false),
+                new Sale("Ketchup", "Mike", 3, 2, "27 Groove St.", false),
+                new Sale("Coffe", "George", 2.5, 1, "234 Cranberry Rd.", true),
+                new Sale("Mustard", "Jack", 2, 3, "507 Blueberry Ln.", false),
+                new Sale("Tea", "Bob", 1, 4, "887 Olive St.", true)
+            };
+
+            SumTotal = TotalProfit(salesList, sale => sale.Item == "Coffe", 
+                sale => (sale.PricePerItem * sale.Quantity) * 0.8, 
+                (sale, profit) => Console.WriteLine("Coffee item for " + sale.Customer + " , total profit: " + profit), 
+                sale => Console.WriteLine("Non - Coffee item, skipping"));
+            Console.WriteLine("Total: " + SumTotal);
+
+            SumTotal = TotalProfit(salesList, sale => sale.Quantity > 1,
+                sale => (sale.ExpeditedShipping ? 20 : 0) + (sale.PricePerItem * sale.Quantity),
+                (sale, profit) => Console.Write(sale.ExpeditedShipping ? "Expedited Shipping sale of " + sale.Item + " -Extra $20 profit\n" : ""),
+                sale => Console.WriteLine("Single Order Item " + sale.Item));
+            Console.WriteLine("Total: " + SumTotal);
+            Console.ReadKey();
+            double TotalProfit(Sale[] sales, Func<Sale, bool> func1, Func<Sale, double> func2, Action<Sale, double> action1, Action<Sale> action2)
+            {
+                double total = 0;
+                foreach (Sale sale in sales)
+                {
+                    if(func1(sale))
+                    {
+                        double profit = func2(sale);
+                        action1(sale, profit);
+                        total += profit;
+                    }
+                    else
+                    {
+                        action2(sale);
+                    }
+                }
+                return total;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCON_368_Asgn04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sale[] Sales = new Sale[10];
+
+            var PricePerItemOver10 =
+                from sale in Sales
+                where sale.PricePerItem > 10.0
+                select sale;
+
+            var PricePerItemOver10Extension = Sales.Where(sale => sale.PricePerItem > 10.0);
+
+            var QuantityIs1 =
+                from sale in Sales
+                where sale.Quantity == 1
+                select sale;
+
+            var QuantityIs1Extension = Sales.Where(sale => sale.Quantity == 1)
+                .OrderByDescending(sale => sale);
+
+            var TeaNoExpeditedShipping =
+                from sale in Sales
+                where sale.Item == "Tea" && !sale.ExpeditedShipping
+                select sale;
+            var TeaNoExpeditedShippingExtension = Sales.Where(sale => sale.Item == "Tea" && !sale.ExpeditedShipping);
+
+            var TotalOrderOver100 =
+                from sale in Sales
+                where (sale.PricePerItem * sale.Quantity) > 100.0
+                select sale;
+
+            var TotalOrderOver100Extension = Sales.Where(sale => (sale.PricePerItem * sale.Quantity) > 100.0);
+
+            var LLCSales =
+                from sale in Sales
+                where sale.Customer.Contains("LLC")
+                select new { sale.Item, TotalPrice = sale.PricePerItem * sale.Quantity, Shipping = sale.Address + (sale.ExpeditedShipping ? "EXPEDITE" : "") };
+
+            var LLCSalesExtension = Sales.Where(sale => sale.Customer.Contains("LLC")).
+                Select(sale => new { sale.Item, TotalPrice = sale.PricePerItem * sale.Quantity, Shipping = sale.Address + (sale.ExpeditedShipping ? "EXPEDITE" : "") });
+        }   
+    }
+
+    public class Sale
+    {
+        public Sale(String Item, String Customer, double PricePerItem, int Quantity, String Address, bool ExpeditedShipping)
+        {
+            this.Item = Item;
+            this.Customer = Customer;
+            this.PricePerItem = PricePerItem;
+            this.Quantity = Quantity;
+            this.Address = Address;
+            this.ExpeditedShipping = ExpeditedShipping;
+        }
+        public String Item { get; set; }
+        public String Customer { get; set; }
+        public double PricePerItem { get; set; }
+        public int Quantity { get; set; }
+        public String Address { get; set; }
+        public bool ExpeditedShipping { get; set; }
+
+    }
+}
